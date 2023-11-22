0d34e5eec2f937337228070ff5ea97e7339c98be
KrakenAura <akarim18641@gmail.com>
2023-11-22 10:52:48 +0700

    tes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pet/firebase_options.dart';
 import 'app/routes/app_pages.dart';
 import 'app/controllers/pet_controller.dart';
 import '../../api/meowfacts_api/facts_controller.dart';
 
-void main() {
-  Get.put(PetController());
+// void main() {
+//   Get.put(PetController());
 
+//   runApp(
+//     GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: "Application",
+//       initialRoute: AppPages.INITIAL,
+//       getPages: AppPages.routes,
+//     ),
+//   );
+// }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
